bcf831c164bc7fd9fc289ea4c721f4f028e0764a
Saurav Kunwar <sauravkunwar63@gmail.com>
2025-02-09 22:09:23 +0545

    first commit

---
+++ src/appwrite.js
@@ -0,0 +1,51 @@
+import { Client, Databases, ID, Query } from "appwrite";
+
+const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
+const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
+const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
+
+const client = new Client()
+  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setProject(PROJECT_ID);
+
+const database = new Databases(client);
+
+export const updateSearchCount = async (searchTerm, movie) => {
+  //use appwrite SDK to check if search term already exist in database, if it does, update the count, but if doesn't exist, create a new document with that search term and count to 1
+  try {
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+      Query.equal("searchTerm", searchTerm),
+    ]);
+
+    //if it does update the count
+    if (result.documents.length > 0) {
+      const doc = result.documents[0];
+      await database.updateDocument(DATABASE_ID, COLLECTION_ID, doc.$id, {
+        count: doc.count + 1,
+      });
+
+      //if it doesn't , create a new document with search term and count as 1
+    } else {
+      await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
+        searchTerm,
+        count: 1,
+        movie_id: movie.id,
+        poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+      });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getTrendingMovies = async () => {
+  try {
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+      Query.limit(5),
+      Query.orderDesc("count"),
+    ]);
+    return result.documents;
+  } catch (error) {
+    console.error(error);
+  }
+};
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//     content: [
+//       "./index.html",
+//       "./src/**/*.{js,ts,jsx,tsx}",
+//     ],
+//     darkMode: "class",
+    
+//      theme: {
+//        extend: {},
+//      },
+    
+//     important: true, // This adds !important to all Tailwind classes
+   
+//     plugins: [],
+//   }
+
